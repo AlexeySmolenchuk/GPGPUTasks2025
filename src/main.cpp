bf3e5b863bd1da99655bdce9f94f01abb6433589
Alexey Smolenchuk <alexey.smolenchuk@gmail.com>
2026-02-18 17:34:03 +0000

    submission

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
+#include "CL/cl_platform.h"
 #include <CL/cl.h>
 #include <libclew/ocl_init.h>
 
+#include <cstddef>
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
@@ -71,15 +73,26 @@ int main()
 		// Аналогично тому, как был запрошен список идентификаторов всех платформ - так и с названием платформы, теперь, когда известна длина названия - его можно запросить:
 		std::vector<unsigned char> platformName(platformNameSize, 0);
 		// clGetPlatformInfo(...);
+		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, platformNameSize, platformName.data(), &platformNameSize));
 		std::cout << "    Platform name: " << platformName.data() << std::endl;
-
+		
 		// TODO 1.3
 		// Запросите и напечатайте так же в консоль вендора данной платформы
+		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VERSION, 0, nullptr, &platformNameSize));
+		std::vector<unsigned char> platformVendor(platformNameSize, 0);
+		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VERSION, platformNameSize, platformVendor.data(), &platformNameSize));
+		std::cout << "    Platform vendor: " << platformVendor.data() << std::endl;
 
 		// TODO 2.1
 		// Запросите число доступных устройств данной платформы (аналогично тому, как это было сделано для запроса числа доступных платформ - см. секцию "OpenCL Runtime" -> "Query Devices")
 		cl_uint devicesCount = 0;
-
+		OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, nullptr, &devicesCount));
+		std::vector<cl_device_id> devices(devicesCount);
+		
+		OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, devicesCount, devices.data(), &devicesCount));
+		
+		
+		
 		for(int deviceIndex = 0; deviceIndex < devicesCount; ++deviceIndex)
 		{
 			// TODO 2.2
@@ -88,6 +101,44 @@ int main()
 			// - Тип устройства (видеокарта/процессор/что-то странное)
 			// - Размер памяти устройства в мегабайтах
 			// - Еще пару или более свойств устройства, которые вам покажутся наиболее интересными
+			size_t size;
+			OCL_SAFE_CALL( clGetDeviceInfo (devices[deviceIndex], CL_DEVICE_NAME, 0, nullptr, &size));
+			std::vector<char> charInfo (size);
+			OCL_SAFE_CALL( clGetDeviceInfo (devices[deviceIndex], CL_DEVICE_NAME, size, charInfo.data(), &size));
+			std::cout << "\t"<< charInfo.data() << std::endl;
+
+			cl_device_type deviceType;
+			OCL_SAFE_CALL( clGetDeviceInfo (devices[deviceIndex], CL_DEVICE_TYPE, sizeof(deviceType), &deviceType, &size));
+			std::cout << "\t";
+			switch(deviceType)
+			{
+				case CL_DEVICE_TYPE_DEFAULT: 		std::cout << "CL_DEVICE_TYPE_DEFAULT"; 		break;
+				case CL_DEVICE_TYPE_CPU: 			std::cout << "CL_DEVICE_TYPE_CPU"; 			break;
+				case CL_DEVICE_TYPE_GPU: 			std::cout << "CL_DEVICE_TYPE_GPU"; 			break;
+				case CL_DEVICE_TYPE_ACCELERATOR:	std::cout << "CL_DEVICE_TYPE_ACCELERATOR"; 	break;
+				case CL_DEVICE_TYPE_CUSTOM: 		std::cout << "CL_DEVICE_TYPE_CUSTOM"; 		break;
+				case CL_DEVICE_TYPE_ALL: 			std::cout << "CL_DEVICE_TYPE_ALL"; 			break;
+			}
+			std::cout << std::endl;
+
+			cl_ulong memsize;
+			OCL_SAFE_CALL( clGetDeviceInfo (devices[deviceIndex], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(cl_ulong), &memsize, &size));
+			std::cout << "\t"<< memsize/1024/1024 << " MB"<< std::endl;
+
+
+			OCL_SAFE_CALL( clGetDeviceInfo (devices[deviceIndex], CL_DRIVER_VERSION, 0, nullptr, &size));
+			charInfo.resize(size);
+			OCL_SAFE_CALL( clGetDeviceInfo (devices[deviceIndex], CL_DRIVER_VERSION, size, charInfo.data(), &size));
+			std::cout << "\tDriver version: "<< charInfo.data() << std::endl;
+			
+			OCL_SAFE_CALL( clGetDeviceInfo (devices[deviceIndex], CL_DEVICE_PROFILE, 0, nullptr, &size));
+			charInfo.resize(size);
+			OCL_SAFE_CALL( clGetDeviceInfo (devices[deviceIndex], CL_DEVICE_PROFILE, size, charInfo.data(), &size));
+			std::cout << "\t"<< charInfo.data() << std::endl;
+
+			size_t workgroups;
+			OCL_SAFE_CALL( clGetDeviceInfo (devices[deviceIndex], CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(size_t), &workgroups, 0));
+			std::cout << "\tMax workgroups: "<< workgroups << std::endl;
 		}
 	}
 
