f28c8c65df90ba10fa05feab86acdefde660a975
Alexey Smolenchuk <alexey.smolenchuk@gmail.com>
2026-02-20 00:51:00 +0000

    add mandelbrot cu

--- src/main_mandelbrot.cpp
+++ src/main_mandelbrot.cpp
@@ -58,7 +58,9 @@ void run(int argc, char** argv)
     // TODO 000 сделайте здесь свой выбор API - если он отличается от OpenCL то в этой строке нужно заменить TypeOpenCL на TypeCUDA или TypeVulkan
     // TODO 000 после этого изучите этот код, запустите его, изучите соответсвующий вашему выбору кернел - src/kernels/<ваш выбор>/aplusb.<ваш выбор>
     // TODO 000 P.S. если вы выбрали CUDA - не забудьте установить CUDA SDK и добавить -DCUDA_SUPPORT=ON в CMake options
-    gpu::Context context = activateContext(device, gpu::Context::TypeOpenCL);
+    // gpu::Context context = activateContext(device, gpu::Context::TypeOpenCL);
+    gpu::Context context = activateContext(device, gpu::Context::TypeCUDA);
+    // gpu::Context context = activateContext(device, gpu::Context::TypeVulkan);
     // OpenCL - рекомендуется как вариант по умолчанию, можно выполнять на CPU, есть printf, есть аналог valgrind/cuda-memcheck - https://github.com/jrprice/Oclgrind
     // CUDA   - рекомендуется если у вас NVIDIA видеокарта, есть printf, т.к. в таком случае вы сможете пользоваться профилировщиком (nsight-compute) и санитайзером (compute-sanitizer, это бывший cuda-memcheck)
     // Vulkan - не рекомендуется, т.к. писать код (compute shaders) на шейдерном языке GLSL на мой взгляд менее приятно чем в случае OpenCL/CUDA
@@ -71,12 +73,13 @@ void run(int argc, char** argv)
     unsigned int width = 2048;
     unsigned int height = 2048;
     unsigned int iterationsLimit = 256;
-    unsigned int isSmoothing = false;
+    unsigned int isSmoothing = true;
 
 #if 1
     float centralX = -0.789136f;
     float centralY = -0.150316f;
-    float sizeX = 0.00239f;
+    // float sizeX = 0.00239f;
+    float sizeX = 0.0239f;
 #else
     // Менее красивый ракурс, но в этом ракурсе виден весь фрактал:
     float centralX = -0.5f;
@@ -126,8 +129,9 @@ void run(int argc, char** argv)
 
                     // _______________________________CUDA___________________________________________
                 } else if (context.type() == gpu::Context::TypeCUDA) {
-                    // TODO cuda::mandelbrot(..);
-                    throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
+                    gpu::WorkSize workSize(GROUP_SIZE_X, GROUP_SIZE_Y, width, height);
+                    cuda::mandelbrot(workSize, gpu_results, width, height, centralX - sizeX / 2.0f, centralY - sizeY / 2.0f, sizeX, sizeY, iterationsLimit, isSmoothing);
+                    // throw std::runtime_error(CODE_IS_NOT_IMPLEMENTED);
 
                     // _______________________________Vulkan_________________________________________
                 } else if (context.type() == gpu::Context::TypeVulkan) {
@@ -160,7 +164,7 @@ void run(int argc, char** argv)
         std::cout << "Mandelbrot effective algorithm GFlops: " << maxApproximateFlops / gflops / stats::median(times) << " GFlops" << std::endl;
 
         // Сохраняем картинку
-        image8u image = renderToColor(cpu_results.ptr(), width, height);
+        image8u image = renderToColor(current_results.ptr(), width, height);
         std::string filename = "mandelbrot " + algorithm + ".bmp";
         std::cout << "saving image to '" << filename << "'..." << std::endl;
         image.saveBMP(filename);
